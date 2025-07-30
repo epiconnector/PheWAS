370cd0c7fd8c639eb2e3688356dd431ab715adf2
Robert Gentleman <rgentlem@gmail.com>
2025-07-30 18:59:32 -0400

    more updating - added a useResiduals flag to generic_enwas so we fit the residuals only models - still needs more graphics etc

--- R/generic-enwas.R
+++ R/generic-enwas.R
@@ -5,6 +5,7 @@
 ## - baseline model formula as character string
 ## - character vector of exposure variables
 
+## add var to the model and get its coefficients from the lm
 summarize_var <- function(var, base_model, data, trans = c("none", "log", "invnorm"))
 {
   trans <- match.arg(trans)
@@ -17,18 +18,41 @@ summarize_var <- function(var, base_model, data, trans = c("none", "log", "invno
   coef(summary(fm))[var, ]
 }
 
+## a version that uses the residuals from the baseline model
+## so the model is much simpler
+summarize_varR = function (var, data, trans = c("none", "log", "invnorm")) 
+{
+    trans <- match.arg(trans)
+    var <- switch(trans, none = var, log = sprintf("log(%s)", 
+        var), invnorm = sprintf("invNorm(%s)", var))
+    fm <- lm(paste0("Residuals ~", var), data = data)
+    coef(summary(fm))[var, ]
+}
+
 add_rownames <- function(d, var = "rownames")
 {
   d[[var]] <- rownames(d)
   d
 }
 
-generic_enwas <- function(data, base_model, expvars, ...)
+generic_enwas <- function(data, base_model, expvars, ..., useResiduals=FALSE)
 {
+  if( useResiduals) {
+     Residuals = lm(base_model, data = data)$residuals
+     data$Residuals = Residuals
+    EV_summary_invnorm <-
+     sapply(expvars, summarize_varR, 
+     data = data, ...) |> t() |> as.data.frame() |> add_rownames("EV") |>
+    dplyr::mutate(LCL = `Estimate` - 2 * `Std. Error`,
+                  UCL = `Estimate` + 2 * `Std. Error`,
+                  adj_pval = p.adjust(`Pr(>|t|)`, "BH"))
+  } else {
   EV_summary_invnorm <- 
     sapply(expvars, summarize_var, base_model = base_model, data = data, ...) |>
     t() |> as.data.frame() |> add_rownames("EV") |>
     dplyr::mutate(LCL = `Estimate` - 2 * `Std. Error`,
                   UCL = `Estimate` + 2 * `Std. Error`,
                   adj_pval = p.adjust(`Pr(>|t|)`, "BH"))
-}
+    }
+  return(EV_summary_invnorm)
+ }
